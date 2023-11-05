25ae195eb260045faf5494f59487575f06813803
VIJAY M <147833719+VIJAY-MAYAVAN@users.noreply.github.com>
2023-11-05 11:55:53 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,50 @@
+let myResume={
+    "basics": {
+      "name": "VIJAY M",
+      "email": "yajivv25@gmail.com",
+      "phone": 6374891253,
+      "degree": "B.Sc",
+      "location": {
+        "address": "66/2 kozhinjipatti ,sendharapatti post",
+        "postalCode": 636110,
+        "city": "salem",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "github":"https://github.com/VIJAY-MAYAVAN"
+        }
+      ]
+    },
+    "education": [
+      {
+        "institution": "Nehru Arts And Science College",
+        "department": "BIOTECHNOLOGY",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2021,
+        "gpa": 5.6,
+      }
+    ],
+    "skills": [
+      {
+        "name": "javascript",
+        "level": "learner",
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,English,Telugu",
+        "read,write,speak":"Tamil,English",
+        "speak":"Telugu"
+      }
+    ],
+    "interests": [
+      {
+        "name": "pencil drawings",
+      }
+    ]
+  }
+  console.log(myResume);
